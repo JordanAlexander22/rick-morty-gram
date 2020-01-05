d924dedc2326b91dd59f75ea847fd43b08779660
jordan <jordan@jordans-MacBook-Air.local>
2020-01-05 16:28:49 +0700

    utilized props to enable future posts

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Header from "./components/Header";
 import Post from "./components/Post";
 class App extends Component {
   render() {
-    return (
-      <div>
+    return <div className="App">
         <Header />
-        <div>
-          <Post />
-        </div>
-      </div>
-    );
+        <section className="App-main">
+          <Post nickname= "Rick Sanchez" avatar="https://i.pinimg.com/originals/5d/32/76/5d32768cfb240ef4875c712f4b2f08ba.jpg" caption= "We gotta go on an adventure Morty! #goodtimes #loveyabud" image= "https://www.goldderby.com/wp-content/uploads/2019/04/Rick-and-Morty.jpg?w=620&h=360&crop=1" />
+        </section>
+      </div>;
   }
 }
 export default App;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
 import "./Post.css";
+
 class Post extends Component {
+  constructor(props){
+    super(props);
+  }
   render() {
+    const nickname= this.props.nickname;
+    const avatar= this.props.avatar;
+    const image = this.props.image;
+    const caption= this.props.caption;
     return (
       <article className="Post" ref="Post">
         <header>
           <div className="Post-user">
             <div className="Post-user-avatar">
               <img
-                src="https://i.pinimg.com/originals/5d/32/76/5d32768cfb240ef4875c712f4b2f08ba.jpg"
-                alt= "rick profile pic"
+                //src="https://i.pinimg.com/originals/5d/32/76/5d32768cfb240ef4875c712f4b2f08ba.jpg"
+                //alt= "rick profile pic"
+                src={avatar} alt= {nickname}
               />
             </div>
             <div className="Post-user-nickname">
-              <span>Rick Sanchez</span>
+              <span>{nickname}</span>
             </div>
           </div>
         </header>
         <div className="Post-image">
           <div className="Post-image-bg">
             <img
-              alt="first pic"
-              src="https://www.goldderby.com/wp-content/uploads/2019/04/Rick-and-Morty.jpg?w=620&h=360&crop=1"
+              //alt="first pic"
+              //src="https://www.goldderby.com/wp-content/uploads/2019/04/Rick-and-Morty.jpg?w=620&h=360&crop=1"
+              alt= {caption} src= {image}
             />
           </div>
         </div>
         <div className="Post-caption">
-          <strong>Rick</strong> We gotta go on an adventure Morty! #goodtimes #loveyabud
+          <strong>{nickname}</strong> {caption}
         </div>
       </article>
     );
