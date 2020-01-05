c56668747b4b31aa24dfb7b123aa0c6777b3994f
jordan <jordan@jordans-MacBook-Air.local>
2020-01-05 15:33:15 +0700

    got header uploaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-  <h1>hello world</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+class App extends Component {
+  render() {
+    return <Header />;
+  }
 }
-
 export default App;
